d08cb9acccbc0afef32ba0b913e214b782763826
Nicholas Nooney <nooney@stolaf.edu>
2016-05-09 16:56:15 -0500

    Cleaned up Story Data

--- data/stry.js
+++ data/stry.js
@@ -1,26 +1,32 @@
 var STORY_DATA = [
   {
     "id": 1,
-    "story": "Woke up in HK, showered and went to Tomson for math class. Went to the cage for a snack, then to Rolvaag to study. Went back to Tomson for Spanish class, then to Stav for lunch. After lunch, I went to the programming lab to TA a class. After class, I went to HOM to warm up for my voice lesson, which was at 4 in CHM. After a productive lesson, I went to the OSA to drop my stuff off before a meeting for Senior Days, in BC 142. I quickly went to Boe at the conclusion of the meeting to attend Studio Class, where I sang. I returned to the OSA for an SGA meeting, then went home to HK, where I attended part of the Hall Council meeting. I ended the day by going back to my room to do homework and go to bed.",
-    "path": 0,
-    "difficulty": 14
+    "story": "I woke up in Hillboe, showered and went to Tomson for math class. After that, I was feeling hungry, so I went to the cage for a snack, then to Rolvaag to study for a biology class. Afterwards, I went back to Tomson for Spanish class, then back to the caf for lunch. After lunch, I went to the Hall of Music to warm up for my voice lesson, which was at 4 in Christiansen Hall of Music. After a productive lesson, I went to the SGA office in Buntrock to drop my stuff off before a meeting in Buntrock Commons 142. I quickly went to Boe Chapel at the conclusion of the meeting to attend Studio Class, where I sang. Then I went home to Hillboe, where I attended part of the Hall Council meeting. I ended the day by going back to my room to do homework and go to bed.",
+    "path": [[285,460],[410,340],[465,235],[495,260],[410,340],[465,255],[335,190],[290,235],[445,235],[410,230],[285,460]],
+    "difficulty": 11
   },
   {
     "id": 2,
-    "story": "I woke up in Ytterboe at 7:45 after having a dream that I had a conversation with a high school crush. I was spooning my girlfriend. I went to Stav Hall at 8AM to have breakfast. I did emails in the SGA Office then went to old main for my class. after class i headed back to the OSA to do more work. then went to lunch in stav hall. Did more SGA work in OSA then went back to my room to charge and call my dad. Then I went to Regents Hall of Natural Sciences and did emails in the Phlounge. Then I went to Applebees for dinner. then I headed back to my room to change for weightlifting down in skog. then I headed to Skog for weightlifting then i went to osa for more work then i went back home to ytt.",
-    "path": 0,
-    "difficulty": 13
+    "story": "I woke up in Ytterboe at 7:45 after having a dream that I had a conversation with a high school crush. I was spooning my girlfriend. I went to Stav Hall at 8AM to have breakfast. I did emails in the SGA Office in Buntrock Commons then went to Old Main for my ethics class. After class I went to lunch in Stav Hall. After lunch I went back to my room to change and call my dad. Then I went to Regents Hall of Natural Sciences and did emails in the Phlounge (Physics Lounge). Then I went to dinner in the caf. After dinner I headed back to my room to change for weightlifting down in Skoglund. Next, I headed to Skoglund for weightlifting then I went back home to Ytterboe.",
+    "path": [[200,240],[465,255],[445,235],[580,395],[465,255],[200,240],[500,400],[465,255],[200,240],[200,100],[200,240]],
+    "difficulty": 11
   },
   {
     "id": 3,
-    "story": "Today I woke up in my room in Ytterboe. After that I packed up my bag and walked to Tomson for stats class. That was from 10:45am-11:40am. After that, I went to lunch. When I went in the caf, the fire alarm went off. I left and walked outside of Buntrock. I talked with friends for a few minutes while waiting for everyone to be allowed inside again. When we went back inside, I went back to the caf and had lunch with a friend. Then, I went back to my room to study for a few hours. After that, I met with my 4th-grade Project Friendship buddy outside of Buntrock, and we walked outside of the natural lands and talked about life. Then, I went to dinner in Buntrock, went to the stats presentations for the CIR, and came back to the pod at night and now I'm typing this.",
-    "path": 0,
-    "difficulty": 10
+    "story": "Today I woke up in my room in Mellby. After that I packed up my bag and walked to Tomson for stats class. That was from 10:45am-11:40am. After that, I went to lunch. When I went in the caf, the fire alarm went off. I left and walked outside of Buntrock. I talked with friends for a few minutes while waiting for everyone to be allowed inside again. When we went back inside, I went back to the caf and had lunch with a friend. Then, I went back to my room to study for a few hours. After that, I met with my 4th-grade Project Friendship buddy outside of Buntrock, and we walked outside of the natural lands and talked about life. Then, I went to dinner in Buntrock, went to the stats presentations for the CIR in Regents Hall, and came back to Mellby at night and went to bed.",
+    "path": [[350,315],[435,365],[465,255],[445,255],[465,255],[350,315],[445,255],[0,0],[465,255],[490,400],[350,315]],
+    "difficulty": 11
   },
   {
     "id": 4,
-    "story": "I woke up in Ytterboe, then went to Stav Hall for breakfast. Afterwards, I worked on homework and answered email in the SGA office until I went to art class in Dittman. After class, I checked my mail in Buntrock then went to Regents Math to meet with a professor for an independent study. From there, I went back to Buntrock to continue working on models from my independent study, until I had Sustained Dialogues at 4:15pm. After that, I tutored another student in the academic support center, then I went to dinner in Stav again. Finally, I went back to the SGA office and then to Ytterboe to finish up homework and go to bed.",
-    "path": 0,
-    "difficulty": 12
+    "story": "I woke up in Thorson, then went to Stav Hall for breakfast. Afterwards, I worked on homework and answered email in the SGA office in Buntrock until I went to art class in Dittman. After class, I checked my mail in Buntrock then went to Regents Math to meet with a professor for an independent study. From there, I went back to Buntrock to continue working on models from my independent study, until I had Sustained Dialogues at 4:15pm in Rolvaag. After that, I tutored another student in the Academic Support Center, then I went to dinner in Stav again. Finally, I went back to the SGA office and then to Thorson to finish up homework and go to bed.",
+    "path": [[575,135],[465,255],[445,235],[365,175],[450,240],[450,375],[500,260],[445,235],[465,255],[445,235],[575,135]],
+    "difficulty": 11
+  },
+  {
+    "id": 5,
+    "story": "I woke up in Rand, then I went to Old Main for Philosophy class, and then to the computer science class room in the tunnel between Regents and Tomson. Then I went to get a bag lunch in Buntrock, and came back to the computer science room. Next, I went to the colloquium in Regents Hall of Natural Science 410. Afterwards, I came back to the CS lab, then I went to dinner in the caf, and then back to Rand.",
+    "path": [[395,45],[580,395],[465,380],[465,230],[465,380],[485,400],[465,380],[465,255],[395,45]],
+    "difficulty": 9
   }
 ]
